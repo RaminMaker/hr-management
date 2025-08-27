3b1dc030413e2ae6d73ab2e737eca8bc86efd9a8
Alex Groff <162096376+alexgroff12@users.noreply.github.com>
2025-08-27 17:24:49 +0330

    Initial commit: Modular project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'departments', loadChildren: () => import('./features/departments/departments.module').then(m => m.DepartmentsModule) }, { path: 'employees', loadChildren: () => import('./features/employees/employees.module').then(m => m.EmployeesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
